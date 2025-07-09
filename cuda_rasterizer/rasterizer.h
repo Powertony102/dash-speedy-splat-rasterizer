61344bb35d85c2c6f1d9c1e954529e3b862f8c00
Powertony102 <powertony102@gmail.com>
2025-07-09 18:53:29 +0800

    Fix declarification - gemini-2

--- cuda_rasterizer/rasterizer.h
+++ cuda_rasterizer/rasterizer.h
@@ -28,6 +28,34 @@ namespace CudaRasterizer
 			float* projmatrix,
 			bool* present);
 
+		static std::tuple<int, int> forward(
+			std::function<char* (size_t)> geometryBuffer,
+			std::function<char* (size_t)> binningBuffer,
+			std::function<char* (size_t)> imageBuffer,
+			const int P, int D, int M,
+			const float* background,
+			const int width, int height,
+			const float* means3D,
+			const float* shs,
+			const float* colors_precomp,
+			const float* opacities,
+			const float* scales,
+			const float scale_modifier,
+			const float* rotations,
+			const float* cov3D_precomp,
+			const float* viewmatrix,
+			const float* projmatrix,
+			const float* cam_pos,
+			const float tan_fovx, float tan_fovy,
+			const bool prefiltered,
+			bool antialiasing,
+			float* kernel_times,
+			float* out_color,
+			int* radii,
+			bool debug,
+			const int tile_size
+		);
+
 		static void backward(
 			const int P, int D, int M, int R,
 			const float* background,
